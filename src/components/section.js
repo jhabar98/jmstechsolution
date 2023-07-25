7827ebc2cc479112b72db1a28357298e57d48c3f
jhabar98 <125726549+jhabar98@users.noreply.github.com>
2023-07-25 22:06:49 +0530

    update

--- src/components/section.js
+++ src/components/section.js
@@ -5,7 +5,7 @@ function section() {
     <div>
        <section id="hero" class="d-flex justify-content-center align-items-center">
     <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
-      <h1>Learning Today,<br/>Leading Tomorrow</h1>
+      <h1>Learning ,<br/>Leading Tomorrow</h1>
       <h2>Vikramshaala Software Foundation providing,
         all web design & development solutions.</h2>
       <a href="courses.html" class="btn-get-started">Get Started</a>
